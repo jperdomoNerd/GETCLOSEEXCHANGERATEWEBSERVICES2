439369e2bb82d1184a8ccc9f46c7823c6affb7f3
Julian Perdomo <jperdomo.nerd@gmail.com>
2025-12-18 16:22:53 -0500

    Add docker and playwright

--- Server.js
+++ Server.js
@@ -1,26 +1,15 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
+const { chromium } = require('playwright');
 
 const app = express();
 
 async function getTend() {
-  const browser = await puppeteer.launch({
-    headless: 'new',
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage'
-    ]
-  });
-
+  const browser = await chromium.launch();
   const page = await browser.newPage();
 
   await page.goto(
     'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
-    {
-      waitUntil: 'networkidle0',
-      timeout: 120000
-    }
+    { waitUntil: 'networkidle' }
   );
 
   const values = await page.$$eval(
@@ -28,29 +17,22 @@ async function getTend() {
     els => els.map(e => e.innerText.trim())
   );
 
-  const todayExchangeRate = parseFloat(values[5].replace(',', ''));
-  const closingExchangeRate = parseFloat(values[13].replace(',', ''));
+  await browser.close();
 
   return {
-    todayExchangeRate,
-    closingExchangeRate
+    todayExchangeRate: parseFloat(values[5].replace(',', '')),
+    closingExchangeRate: parseFloat(values[13].replace(',', ''))
   };
 }
 
-// routing
 app.get('/', async (req, res) => {
   try {
-    const data = await getTend();
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: `xInternal Server Error ${err.message}` });
+    res.json(await getTend());
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: `Internal Server Error: ${e.message}` });
   }
 });
 
-// start server
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server started on port 3000');
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
